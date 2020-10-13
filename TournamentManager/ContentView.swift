5e9e84a28fe024489b9c1e472586a1ece14beef4
Dix Lorenz <dlorenz@medimach.de>
2020-10-13 11:57:49 +0200

    Initial Commit

---
+++ TournamentManager/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TournamentManager
+//
+//  Created by Dix Lorenz on 13.10.20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TournamentManager/TournamentManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TournamentManagerApp.swift
+//  TournamentManager
+//
+//  Created by Dix Lorenz on 13.10.20.
+//
+
+import SwiftUI
+
+@main
+struct TournamentManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
